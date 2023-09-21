8c6aa3d6bfcfde755a9250eba226f09a814a0f75
Joel Speed <joel.speed@hotmail.co.uk>
2023-09-21 15:17:52 +0100

    Add addons API

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	kubeadmbootstrapv1beta1 "sigs.k8s.io/cluster-api/bootstrap/kubeadm/api/v1beta1"
 	clusterctlv1alpha3 "sigs.k8s.io/cluster-api/cmd/clusterctl/api/v1alpha3"
 	kubeadmcontrolplanev1beta1 "sigs.k8s.io/cluster-api/controlplane/kubeadm/api/v1beta1"
+	addonsv1beta1 "sigs.k8s.io/cluster-api/exp/addons/api/v1beta1"
 	expv1beta1 "sigs.k8s.io/cluster-api/exp/api/v1beta1"
 	ipamv1alpha1 "sigs.k8s.io/cluster-api/exp/ipam/api/v1alpha1"
 	runtimev1alpha1 "sigs.k8s.io/cluster-api/exp/runtime/api/v1alpha1"
@@ -31,6 +32,10 @@ func main() {
 	myMachinePool := &expv1beta1.MachinePool{}
 
 	fmt.Printf("My MachinePool: %+v\n", myMachinePool)
+
+	myClusterResourceSet := &addonsv1beta1.ClusterResourceSet{}
+
+	fmt.Printf("My ClusterResourceSet: %+v\n", myClusterResourceSet)
 
 	myIPAddressClaim := &ipamv1alpha1.IPAddressClaim{}
 
